9ff036ed106a939825060fb9a094b6a4075b320e
Boris <biznis.profil@hotmail.com>
2016-02-17 12:57:43 +0100

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@
 
 function validateFormName() {
     var x = document.getElementById("contactname").value;
-    if (x == null || x == "") {
+    if (x === null || x === "") {
         document.getElementById("error").innerHTML = '<span class="label label-danger">Name field cannot be empty</span>';
         return false;
     }
@@ -16,7 +16,7 @@ function focusFunctionName() {
 
 function validateFormPhone() {
     var x = document.getElementById("phone").value;
-    if (x == null || x == "") {
+    if (x === null || x === "") {
         document.getElementById("error1").innerHTML = '<span class="label label-danger">Phone field cannot be empty</span>';				
         return false;
     }
@@ -35,7 +35,7 @@ function focusFunctionPhone() {
 
 function validateFormEmail() {
     var x = document.getElementById("email").value;
-    if (x == null || x == "") {
+    if (x === null || x === "") {
         document.getElementById("error2").innerHTML = '<span class="label label-danger">Email field must be filled out</span>';
         return false;
     }
